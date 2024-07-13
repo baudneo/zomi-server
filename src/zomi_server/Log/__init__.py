36e22d9ba2f7bac1d32cc8005fc2fe628f0e2f1f
baudneo <86508179+baudneo@users.noreply.github.com>
2024-07-13 13:34:19 -0600

    Fix logging formatter

--- src/zomi_server/Log/__init__.py
+++ src/zomi_server/Log/__init__.py
@@ -3,6 +3,6 @@ import logging
 
 SERVER_LOGGER_NAME = "ML:API"
 SERVER_LOG_FORMAT = logging.Formatter(
-    "'%(asctime)s.%(msecs)04d' %(name)s[%(process)s] %(levelname)s %(module)s:%(lineno)d -> %(message)s",
+    "%(asctime)s.%(msecs)d %(name)s %(levelname)s[%(module)s:%(lineno)d]> %(message)s",
     "%m/%d/%y %H:%M:%S",
 )
